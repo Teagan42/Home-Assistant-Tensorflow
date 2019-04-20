7a426e11e9dfe20cb07a872afb1d08c1816a873f
Teagan Glenn <that@teagantotally.rocks>
2019-04-20 13:06:57 -0600

    Use strech libraries

--- startup.sh
+++ startup.sh
@@ -4,13 +4,7 @@ CONFIG_DIR=${1:-"/config"}
 cd /tmp
 
 # Install LIBC6
-curl -OL http://archive.ubuntu.com/ubuntu/pool/main/g/glibc/libc6_2.27-3ubuntu1_amd64.deb
-curl -OL http://archive.ubuntu.com/ubuntu/pool/main/g/glibc/libc6-dev_2.27-3ubuntu1_amd64.deb
-curl -OL http://archive.ubuntu.com/ubuntu/pool/main/g/glibc/libc-bin_2.27-3ubuntu1_amd64.deb
-
-apt-get install -fy ./libc6_2.27-3ubuntu1_amd64.deb
-apt-get install -fy ./libc6-dev_2.27-3ubuntu1_amd64.deb
-apt-get install -fy ./libc-bin_2.27-3ubuntu1_amd64.deb
+apt-get install -fy libc6 libc6-dev libc6-dev-bin libc-bin git unzip
 
 # Clone the latest code from GitHub
 /usr/bin/git clone --depth 1 https://github.com/tensorflow/models.git tensorflow-models
