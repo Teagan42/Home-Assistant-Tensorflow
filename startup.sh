c43cd129a8648a8d24aa2b07a6014bcef39bdd6a
Teagan Glenn <that@teagantotally.rocks>
2019-04-20 12:49:13 -0600

    Full paths

--- startup.sh
+++ startup.sh
@@ -3,8 +3,6 @@ CONFIG_DIR=${1:-"/config"}
 
 cd /tmp
 
-apt-get  update && apt-get install -fy git unzip vim
-
 # Install LIBC6
 curl -OL http://archive.ubuntu.com/ubuntu/pool/main/g/glibc/libc6_2.27-3ubuntu1_amd64.deb
 curl -OL http://archive.ubuntu.com/ubuntu/pool/main/g/glibc/libc6-dev_2.27-3ubuntu1_amd64.deb
@@ -15,11 +13,11 @@ apt-get install -fy ./libc6_2.27-3ubuntu1_amd64.deb
 apt-get install -fy ./libc6-dev_2.27-3ubuntu1_amd64.deb
 
 # Clone the latest code from GitHub
-git clone --depth 1 https://github.com/tensorflow/models.git tensorflow-models
+/usr/bin/git clone --depth 1 https://github.com/tensorflow/models.git tensorflow-models
 
 # download protobuf 3.4
 curl -OL https://github.com/google/protobuf/releases/download/v3.4.0/protoc-3.4.0-linux-x86_64.zip
-unzip -a protoc-3.4.0-linux-x86_64.zip -d protobuf
+/usr/bin/unzip -a protoc-3.4.0-linux-x86_64.zip -d protobuf
 mv protobuf/bin /tmp/tensorflow-models/research
 
 # Build the protobuf models
