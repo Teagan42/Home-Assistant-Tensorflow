99180ca5ee6cec8a46e4aeb77f99a9aa7412cfb6
Teagan Glenn <that@teagantotally.rocks>
2019-04-20 12:46:10 -0600

    curl

--- startup.sh
+++ startup.sh
@@ -3,12 +3,12 @@ CONFIG_DIR=${1:-"/config"}
 
 cd /tmp
 
-apt-get update && apt-get install -fy git unzip vim
+apt-get  update && apt-get install -fy git unzip vim
 
 # Install LIBC6
 curl -OL http://archive.ubuntu.com/ubuntu/pool/main/g/glibc/libc6_2.27-3ubuntu1_amd64.deb
 curl -OL http://archive.ubuntu.com/ubuntu/pool/main/g/glibc/libc6-dev_2.27-3ubuntu1_amd64.deb
-http://archive.ubuntu.com/ubuntu/pool/main/g/glibc/libc-bin_2.27-3ubuntu1_amd64.deb
+curl -OL http://archive.ubuntu.com/ubuntu/pool/main/g/glibc/libc-bin_2.27-3ubuntu1_amd64.deb
 
 apt-get install -fy ./libc-bin_2.27-3ubuntu1_amd64.deb
 apt-get install -fy ./libc6_2.27-3ubuntu1_amd64.deb
